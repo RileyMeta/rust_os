e588b49aa18e7314c97479b45a691d0c778516c6
Riley <32332593+RileyMeta@users.noreply.github.com>
2024-12-11 17:03:05 -0500

    Update lib.rs
    -+ corrected htl -> hlt (halt)

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
 
-    htl_loop();
+    hlt_loop();
 }
 
 pub fn init() {
@@ -58,7 +58,7 @@ pub fn init() {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    htl_loop();
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -85,7 +85,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 // HLT
-pub fn htl_loop() -> ! {
+pub fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
