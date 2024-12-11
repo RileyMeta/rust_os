48a935a62b63524ab6cc51d675f83a868606795d
Riley <32332593+RileyMeta@users.noreply.github.com>
2024-12-11 18:48:33 -0500

    Update lib.rs
    + Added a call to the new memory system 
    -+ Reworked the main function | no longer required `extern "C"`...

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,12 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 
 use x86_64::instructions::hlt;
 
@@ -52,10 +56,13 @@ pub fn init() {
     x86_64::instructions::interrupts::enable();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 // Entry point for 'cargo test'
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+
     init();
     test_main();
     hlt_loop();
