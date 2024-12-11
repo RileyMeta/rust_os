402bcc7427cf28e1f98b638e18bff9d23da16be8
Riley <32332593+RileyMeta@users.noreply.github.com>
2024-12-11 10:06:23 -0500

    Update main.rs
    + Added 3 new test related flags        
    + Mod serial allows us to use the serial.rs driver
    + Flagged the panic handler to not be used during tests
    + Created a new panic handler specifically for tests 
    + Added a test runner that will run tests 
    + Added a test flag to our _start function
    + Added a trivial assertion (1 = 1)
    + Added QEMU exit hack

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 #![no_std] // Specify no Standard Library
 #![no_main] // Specify no traditional main()
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]        // Test Panic Handler (prints to serial out)
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
 }
 
 pub fn logo() {
@@ -20,9 +43,52 @@ pub fn logo() {
     println!(r"|_|  \_\__,_|___/\__|\____/|_____/ ");  
 }
 
-#[no_mangle]
+#[no_mangle]    // The true start of our kernel
 pub extern "C" fn _start() -> ! {
     logo();
+    
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+// Trivial Assertion is a basic test
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);                   // Assert that 1 = 1
+}
+
+
+// QEMU EXIT
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+// Test Traits
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]")
+    }
 }
