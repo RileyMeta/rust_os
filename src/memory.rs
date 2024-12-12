3195620e43ccacd56c86478a910f6409a25083b8
Riley <32332593+RileyMeta@users.noreply.github.com>
2024-12-11 20:12:39 -0500

    Update memory.rs
    - Removed bad debug code

--- src/memory.rs
+++ src/memory.rs
@@ -95,22 +95,3 @@ unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
         frame
     }
 }
-
-
-// Temp Code !TESTING ONLY!
-pub fn create_example_mapping(
-    page: Page,
-    mapper: &mut OffsetPageTable,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
-
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE;
-
-    let map_to_result = unsafe {
-        // FIX ME! THIS IS NOT PROPER OR SAFE
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush();
-}
