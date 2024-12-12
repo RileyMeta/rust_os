69a13fe4ec21e1194c9d24b5d81f5ce8dcfcb926
Riley <32332593+RileyMeta@users.noreply.github.com>
2024-12-11 20:18:47 -0500

    Update main.rs
    - Removed redundant debug code
    - Reassigned the unused boot_info -> _boot_info (this may break something, idk)

--- src/main.rs
+++ src/main.rs
@@ -35,28 +35,10 @@ pub fn logo() {
 
 entry_point!(kernel_main);     // The true start of our kernel
 
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory;
-    use rust_os::memory::BootInfoFrameAllocator;
-    use x86_64::{structures::paging::Page, VirtAddr};
-
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
     logo();     // Simple RustOS logo to show that it's loaded.
     
     rust_os::init();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
-
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // Write the string "New!" to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     // !IMPORTANT! This stuff works, don't touch.
     #[cfg(test)]
